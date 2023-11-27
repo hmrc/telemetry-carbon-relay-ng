057324f8eb75f1c12a0359232b9de33fa8d008af
rizinaa99 <66684341+rizinaa99@users.noreply.github.com>
2023-11-27 17:14:05 +0000

    TEL-4096 matching test and live  setups by simplifying and standardising configuration

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,3 @@
 FROM ${DOCKERHUB}grafana/carbon-relay-ng
 
-COPY init.sh /init.sh
-RUN chmod +x /init.sh
-
-COPY templates/carbon-relay-ng.ini /etc/carbon-relay-ng.ini
-
-CMD ["/init.sh"]
+COPY templates/carbon-relay-ng.ini /conf/carbon-relay-ng.ini
--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -e "s|{{CARBON_CLICKHOUSE_HOST_SHARD_1}}|${CARBON_CLICKHOUSE_HOST_SHARD_1}|" \
-    -e "s|{{CARBON_CLICKHOUSE_HOST_SHARD_2}}|${CARBON_CLICKHOUSE_HOST_SHARD_2}|" \
-    templates/carbon-relay-ng.ini > conf/carbon-relay-ng.ini
-
-
-exec ${DOCKERHUB}grafana/carbon-relay-ng
